45c42f4b4e6f702626a8e07abf3a437e6c1872c5
enerzilius <efblouback@gmail.com>
2026-03-27 15:22:55 -0300

    2 feita

--- lab2/exec.c
+++ lab2/exec.c
@@ -2,19 +2,25 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int main(int argv, char** argc) {
-    if(argv < 2) {
+int main(int argc, char** argv) {
+    if(argc < 2) {
         printf("argumentos insuficientes");
         exit(0);
     }
 
-    char* arguments[argv-1];
-    for(int i = 1; i < argv; ++i) {
-        arguments[i-1] = argc[i];
+    char* arguments[argc];
+    for(int i = 1; i < argc; ++i) {
+        arguments[i-1] = argv[i];
     }
 
-    char buffer[20];
-    sprintf(buffer, "/bin/%s", arguments[0]);
+    arguments[argc-1] = (char*)NULL;
+
+    char src[20];
+    sprintf(src, "/bin/%s", arguments[0]);
+    printf("%s\n", src);
+    
+    printf("PID: %d\n", getpid());
+    execv(src, arguments);
 
     exit(0);
 }
