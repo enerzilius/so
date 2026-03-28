ba51a788a2b86506a9556cdd81f7e9349bdf86df
enerzilius <efblouback@gmail.com>
2026-03-28 00:28:02 -0300

    fazendo o comando rodar como processo filho

--- lab2/exec.c
+++ lab2/exec.c
@@ -1,26 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 int main(int argc, char** argv) {
-    if(argc < 2) {
-        printf("argumentos insuficientes");
-        exit(0);
-    }
+  if(argc < 2) {
+      printf("argumentos insuficientes");
+      exit(0);
+  }
 
-    char* arguments[argc];
-    for(int i = 1; i < argc; ++i) {
-        arguments[i-1] = argv[i];
-    }
+  char* arguments[argc];
+  for(int i = 1; i < argc; ++i) {
+      arguments[i-1] = argv[i];
+  }
 
-    arguments[argc-1] = (char*)NULL;
+  arguments[argc-1] = (char*)0;
 
-    char src[20];
-    sprintf(src, "/bin/%s", arguments[0]);
-    printf("%s\n", src);
-    
-    printf("PID: %d\n", getpid());
-    execv(src, arguments);
-
+  pid_t pid;   
+  pid = fork();
+  if(pid < 0) {
+    printf("problema no fork");
     exit(0);
+  }
+  if(pid == 0) {
+    char src[20];                          
+    sprintf(src, "/bin/%s", arguments[0]); 
+    printf("%s\n", src);
+    execv(src, arguments);
+    exit(0);
+  }
+
+  if(pid > 0) {
+    int status;
+    pid = wait(&status);
+    printf("Processo %d foi finalizado com estado %d. \n", pid, status);
+  }
+
+  exit(0);
 }
