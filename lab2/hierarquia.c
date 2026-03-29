f50005f77d570da3617b45e009fc84cd2c2cbde5
enerzilius <efblouback@gmail.com>
2026-03-29 20:04:40 -0300

    1 aceitável?

--- lab2/hierarquia.c
+++ lab2/hierarquia.c
@@ -4,6 +4,14 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+void showPsTree(pid_t og_pid) {
+  pid_t curr_pid = getpid();
+  //if(curr_pid != og_pid) return;
+  char pid_str[10];                              
+  snprintf(pid_str, 10, "%d", curr_pid);
+  execl("/bin/pstree", "pstree", "-p", "-c", "-T", pid_str, (char*)NULL); 
+}
+
 int main(int argc, char* argv[]) {
   if(argc != 2) {
     printf("argumentos insuficientes");
@@ -15,17 +23,24 @@ int main(int argc, char* argv[]) {
     exit(0);
   }
 
-  // pid_t root_pid = getpid();
+  pid_t og_pid = getpid();
 
-  for(int i = 0; i < n; ++i) {
+ for(int i = 0; i < n-1; ++i) {
     pid_t pid = fork();
     if(pid < 0) exit(0);
+    if(pid > 0) pid = fork();
+    if(pid < 0) exit(0);
+
+    if(pid == 0) {
+      continue;
+    }
+
+    if(pid > 0) {
+      showPsTree(og_pid);
+      //if(i == n-1) showPsTree();
+      exit(0);
+    };
   }
 
-  // char pid_str[16];
-  // system("pstree -c -T", pid_str, NULL);
-  // printf("\n\n");
-
-  wait(NULL);
   exit(0);
 }
