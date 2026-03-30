b6284d0e2d993f16734613c6725a4bf5c483c941
enerzilius <efblouback@gmail.com>
2026-03-30 17:35:14 -0300

    comentario

--- lab2/forkbomb.c
+++ lab2/forkbomb.c
@@ -3,6 +3,14 @@
 #include <unistd.h>
 #include <sys/types.h>
 
+/*                                                                              
+ * Autor: Eber Felipe Barrotti Louback                                          
+ * Descrição: Criar filhos recursivamente, infinitamente                                 
+ * Data de criação: 30/03/2026                                                  
+ * Data de modificação: 30/03/2026                                              
+*/                                                                              
+
+
 void recursiveForks() {
   pid_t pid = fork();
   if(pid<0) exit(0);
