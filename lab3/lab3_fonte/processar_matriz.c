17f9c0c8cc0d3a430916067dc0150ad720eea7c9
enerzilius <efblouback@gmail.com>
2026-04-12 18:03:25 -0300

    sem erros de compilação

--- lab3/lab3_fonte/processar_matriz.c
+++ lab3/lab3_fonte/processar_matriz.c
@@ -11,9 +11,10 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <math.h>
+#include "matriz.h"
 
-int* *mediaAritmetica(void* params);
-int* *mediaGeometrica(void* params);
+void *mediaAritmetica(void* params);
+void *mediaGeometrica(void* params);
 //void criarArquivoTexto(int matriz[][]);
 
 // struct para a thread, marca a linha/coluna que começa,
@@ -25,9 +26,9 @@ struct dataChunk {
 
 // dados retornados das threads
 struct returnData {
-  int posicao, quantidade;
+  int inicio, quantidade;
   int* resultado;
-}
+};
 
 int main(int argc, char* argv[]) {
   if(argc < 2) {
@@ -45,24 +46,25 @@ int main(int argc, char* argv[]) {
   // cria uma matriz lendo um arquivo
   int linhas = 2;
   int colunas = 3; 
-  int matriz[][] = read_matrix_from_file("data_matriz_100_200.in", &linhas, &colunas);
-  print_matrix(matrix, r, c);
+  int **matriz = read_matrix_from_file("data_matriz_100_200.in", &linhas, &colunas);
+  print_matrix(matriz, linhas, colunas);
 
   // divisão do trabalho, metade das threads faz a media aritmetica e a outra faz a geométrica
   // define o ínicio (linha/coluna) da thread e quantas ela vai percorrer
   // se só tiver 1 thread não divide
-  struct dataChunk[numeroThreads];
-  int metadeThreads = numeroThreads/2;
+  struct dataChunk dados[quantidadeThreads];
+  int metadeThreads = quantidadeThreads/2;
   if(metadeThreads < 1) metadeThreads = 1;
   if(quantidadeThreads > 1) {
     int thread = 0;
 
     int inicio = 0;
     int linhasPorThread = linhas/metadeThreads;
-    for(thread < metadeThreads; thread++) {
-      dataChunk[thread].inicio = inicio;
-      dataChunk[thread].workload = linhasPorThread;
-      dataChunk[thread].tamanho = colunas;
+    for(int i = 0; i < metadeThreads; i++) {
+      dados[thread].inicio = inicio;
+      dados[thread].workload = linhasPorThread;
+      if(i == metadeThreads-1) dados[thread].workload += linhas%metadeThreads; 
+      dados[thread].tamanho = colunas;
       inicio += linhasPorThread;
       thread++;
     }
@@ -70,59 +72,84 @@ int main(int argc, char* argv[]) {
     inicio = 0;
     int colunasPorThread = colunas/metadeThreads;
     for(int i = 0; i < metadeThreads; i++) {
-      dataChunk[thread].inicio = inicio;
-      dataChunk[thread].workload = colunasPorThread;
-      if(i == metadeThreads-1) dataChunk[thread].workload += colunas%metadeThreads;
-      dataChunk[thread].tamanho = linhas;
+      dados[thread].inicio = inicio;
+      dados[thread].workload = colunasPorThread;
+      if(i == metadeThreads-1) dados[thread].workload += colunas%metadeThreads;
+      dados[thread].tamanho = linhas;
       inicio += colunasPorThread;
       thread++;
     }
   }
 
   // cria as threads, ajustando caso só crie uma
-  pthread_t threads[numeroThreads];
+  pthread_t threads[quantidadeThreads];
   for(int i = 0; i < metadeThreads; i++) pthread_create(&threads[i], NULL, mediaAritmetica, &dados[i]);
-  if(numeroThreads == 1) metadeThreads = 0;
-  for(int i = metadeThreads; i < numeroThreads; i++) pthread_create(&threads[i], NULL, mediaGeometrica, &dados[i]);
+  int comecoGeometrica = metadeThreads;
+  if(quantidadeThreads == 1) comecoGeometrica = 0;
+  for(int i = metadeThreads; i < quantidadeThreads; i++) pthread_create(&threads[i], NULL, mediaGeometrica, &dados[i]);
   
   int mediaLinhas[linhas];
   for(int i = 0; i < metadeThreads; i++) {
-    struct returnValue retVal; 
-    pthread_join(threads[i], &retVal);
+    struct returnData *retVal; 
+    pthread_join(threads[i], (void**)retVal);
     for(int j = 0; j < retVal->quantidade; j++) {
-      //mediaLinhas[retVal->]
+      mediaLinhas[retVal->inicio+j] = retVal->resultado[j];
     }
   }
+
+  int mediaColunas[colunas];
+  for(int i = comecoGeometrica; i < quantidadeThreads; i++) {
+    struct returnData *retVal;
+    pthread_join(threads[i], (void**)retVal);
+    for(int j = 0; j < retVal->quantidade; j++) {
+      mediaColunas[retVal->inicio+j] = retVal->resultado[j];
+    }
+  }
+
+  printf("aritmetica: ");
+  for(int i = 0; i < linhas; i++) printf("%d, ", mediaLinhas[i]);
+  printf("\n");
+  printf("geometrica: ");
+  for(int i = 0; i < colunas; i++) printf("%d, ", mediaColunas[i]);
+  printf("\n");
+
+  pthread_exit(NULL);
 
   exit(0);
 }
 
-int* *mediaAritmetica(void* params) {
+void *mediaAritmetica(void* params) {
   struct dataChunk* dados = params;
   
-  int results[workload];
-  for(int i = 0; i < dataChunk->workload; i++) {
+  int results[dados->workload];
+  for(int i = 0; i < dados->workload; i++) {
     int sum = 0;
-    for(int j = 0; j < dataChunk->tamanho; j++) sum +=dataChunk->matriz[inicio+i][j];
-    results[i] = sum/tamanho;
+    for(int j = 0; j < dados->tamanho; j++) sum += dados->matriz[dados->inicio+i][j];
+    results[i] = sum/dados->tamanho;
   }
 
-  struct returnValue retVal = {dataChunk.inicio, dataChunk.workload, results};
+  struct returnData *retVal = malloc(sizeof(struct returnData));
+  retVal->inicio = dados->inicio;
+  retVal->quantidade = dados->workload;
+  retVal->resultado = results;
   
   pthread_exit((void*)retVal);
 }
 
-int* *mediaGeometrica(void* params) {
+void *mediaGeometrica(void* params) {
   struct dataChunk* dados = params;
 
-  int results[workload];
-  for(int i = 0; i < dataChunk->workload; i++) {
+  int results[dados->workload];
+  for(int i = 0; i < dados->workload; i++) {
     int produto = 0;
-    for(int j = 0; j < dataChunk->tamanho; j++) produto +=dataChunk->matriz[inicio+i][j];
-    results[i] = pow(produto, 1.0/tamanho);
+    for(int j = 0; j < dados->tamanho; j++) produto += dados->matriz[dados->inicio+i][j];
+    results[i] = pow(produto, 1.0/dados->tamanho);
   }
 
-  struct returnValue retVal = {dataChunk.inicio, dataChunk.workload, results};
+  struct returnData *retVal = malloc(sizeof(struct returnData));
+  retVal->inicio = dados->inicio;
+  retVal->quantidade = dados->workload;
+  retVal->resultado = results;
 
   pthread_exit((void*)retVal);
 }
