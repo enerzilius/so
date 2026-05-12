ecbe68bb7d780d012e425c84908e811c9714ffd8
enerzilius <efblouback@gmail.com>
2026-05-12 14:23:43 -0300

    jantar dos selvagens funcionando

--- problemas_classicos/jantar_selvagens.c
+++ problemas_classicos/jantar_selvagens.c
@@ -1,10 +1,10 @@
-#include <stdlib.h>
-#include <unistd.h>
+#include <errno.h> /* Errors */
 #include <pthread.h>
 #include <semaphore.h>
-#include <sys/types.h>  /* Primitive System Data Types */ 
-#include <errno.h>      /* Errors */
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h> /* Primitive System Data Types */
+#include <unistd.h>
 
 #define N_SELVAGENS 3
 #define PORCOES 6
@@ -20,62 +20,66 @@ void servir();
 void comer();
 void encher_caldeirao();
 
-int main(){
-    pthread_t threads[N_SELVAGENS+1];
-    pthread_attr_t attr;
+int main() {
+  pthread_t threads[N_SELVAGENS + 1];
+  pthread_attr_t attr;
 
-    sem_init(&servindo, 1, 1);
-    sem_init(&caldeirao_vazio, 1, 1);
-    sem_init(&caldeirao_tem_coisa, 1, 0);
+  sem_init(&servindo, 1, 1);
+  sem_init(&caldeirao_vazio, 1, 1);
+  sem_init(&caldeirao_tem_coisa, 1, 0);
 
-    /* criar as threads */
-    pthread_create (&threads[0], NULL, (void *) &cozinheiro, NULL);
-    for (int i=1; i <= N_SELVAGENS; i++) {
-        pthread_create (&threads[i], NULL, (void *) &selvagem, NULL);
-    }
+  /* criar as threads */
+  pthread_create(&threads[0], NULL, (void *)&cozinheiro, NULL);
+  for (int i = 1; i <= N_SELVAGENS; i++) {
+    pthread_create(&threads[i], NULL, (void *)&selvagem, NULL);
+  }
 
-    for (int i=0; i <= N_SELVAGENS; i++) {
-        pthread_join(threads[i], NULL);
-    }
+  for (int i = 0; i <= N_SELVAGENS; i++) {
+    pthread_join(threads[i], NULL);
+  }
 
-    sem_destroy(&servindo); /* destroy semaphore */
-    sem_destroy(&caldeirao_vazio); /* destroy semaphore */
-    sem_destroy(&caldeirao_tem_coisa); /* destroy semaphore */
+  sem_destroy(&servindo);            /* destroy semaphore */
+  sem_destroy(&caldeirao_vazio);     /* destroy semaphore */
+  sem_destroy(&caldeirao_tem_coisa); /* destroy semaphore */
 
-    exit(EXIT_SUCCESS);
+  exit(EXIT_SUCCESS);
 }
 
 void *cozinheiro() {
-    while(1) {
-        sem_wait(&caldeirao_vazio);
-        encher_caldeirao();   
-    }
+  while (1) {
+    sem_wait(&caldeirao_vazio);
+    encher_caldeirao();
+  }
 }
 
 void *selvagem() {
-    while(1) {
-        servir();
-        comer();
-    }
+  while (1) {
+    servir();
+    comer();
+  }
 }
 
 void servir() {
-    sem_wait(&servindo);
-    sem_wait(&caldeirao_tem_coisa);
-    printf("Selvagem se servindo!\n");
-    sleep(1);
-    sem_post(&servindo);
+  sem_wait(&servindo);
+  sem_wait(&caldeirao_tem_coisa);
+  printf("Selvagem se servindo!\n");
+  sleep(1);
+  porcoes--;
+  sem_post(&servindo);
+  printf("Porcoes restantes: %d\n", porcoes);
+  if (porcoes == 0)
     sem_post(&caldeirao_vazio);
 }
 
 void comer() {
-    printf("comeno dog\n");
-    sleep(1);
+  printf("comendo\n");
+  sleep(1);
 }
 
 void encher_caldeirao() {
-    printf("enchendo caldeirao...\n");
-    for(int i = 0; i < PORCOES; ++i) {
-        sem_post(&caldeirao_tem_coisa);
-    }
+  printf("enchendo caldeirao...\n");
+  for (int i = 0; i < PORCOES; ++i) {
+    porcoes++;
+    sem_post(&caldeirao_tem_coisa);
+  }
 }
